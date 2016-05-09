f00434b031ef21e11d1c2a7dc69e879e848c6ed7
Alex Eickhoff <alex@artbanause.de>
2016-05-09 12:12:09 +0200

    mike level power meter

--- applet/src/ambe.c
+++ applet/src/ambe.c
@@ -18,13 +18,25 @@
 #include "gfx.h"
 
 
-
+int max_level;
 
 int ambe_encode_thing_hook(char *a1, int a2, int *a3, int a4,
 		      short a5, short a6, short a7, int a8){
-  //printf("AMBE2+ function is hooked!\n");
+
+  short *s8;
+  int i=0;
+  int max=0;
   
-  //Call back to the original function.
+  s8=(short *)a3; 
+  
+  for (i=0; i<80; i++) {
+    if ( s8[i] > max ) {
+      max=s8[i];
+    }  
+  }
+  
+  max_level=max;
+  
   return ambe_encode_thing(a1,a2,a3,a4,
 			   a5,a6,a7,a8);
 }
--- applet/src/rtc_timer.c
+++ applet/src/rtc_timer.c
@@ -1,4 +1,4 @@
-/*! \file gfx.c
+/**md380_f_4225_operatingmode! \file gfx.c
   \brief wrapper functions for the "RTC Timer"-Task.
 */
 
@@ -10,6 +10,7 @@
 #include "printf.h"
 #include "string.h"
 #include "addl_config.h"
+#include "ambe.h"
 
 // this hook switcht of the exit from the menu in case of RX
 void * f_4225_internel_hook() {
@@ -67,4 +68,67 @@ void f_4102_hook() {
   __asm__("mov %0,r13" : "=r" (sp));
   printf("Call md380_f_4102 from r: %x s: %x\n", return_addr,sp);
   md380_f_4102();
+}
+
+
+void f_4225_hook() {
+  static int y=0;
+  static int bla;
+  int blabla;
+  static blablabla;
+  int i;  
+  // 20000
+  if (bla != max_level) {
+    if ( *md380_f_4225_operatingmode == 0x11 && max_level < 4500 ) {
+      bla = max_level;
+      i=0;
+      blabla=bla;
+      do {
+        blabla/=2;
+        i++;
+      } while (blabla > 2);
+  
+      if ( blablabla != i ) {
+        blablabla=i;
+      gfx_set_fg_color(0x999999);  
+      gfx_set_bg_color(0xff000000);
+      gfx_blockfill( 9, 49, 111 , 56);  
+      gfx_blockfill( 9, 59, 131 , 66);  
+
+      if (max_level > 4000) {
+        gfx_set_fg_color(0x0000ff);
+      } 
+      else if (max_level > 1000 ) {
+        gfx_set_fg_color(0x00ff00);
+      } else {
+        gfx_set_fg_color(0x555555);
+      }
+      gfx_set_bg_color(0xff000000);
+
+      y = max_level/50;
+
+      if (y > 99 ) {
+         y = 99;
+      }
+      if (i>13)i=13;
+
+      gfx_blockfill( 10, 60, i*10 + 10 , 65);
+      gfx_blockfill( 10, 50, y + 10 , 55);
+
+      gfx_set_fg_color(0xff8032);
+      gfx_set_bg_color(0xff000000);
+      }
+    }
+  }
+  
+  if ( *md380_f_4225_operatingmode == 0x12 && bla > 0) {
+    gfx_set_fg_color(0x999999);     
+    gfx_set_bg_color(0xff000000);   
+    gfx_blockfill( 9, 49, 111 , 56);
+    gfx_blockfill( 9, 59, 131 , 66);
+    gfx_set_fg_color(0xff8032);  
+    gfx_set_bg_color(0xff000000);
+   }
+    
+  md380_f_4225 ();  
 }
