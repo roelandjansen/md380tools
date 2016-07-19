afc11c5904ce01b372c57e04b0b9a9b58c8636b2
Alex Eickhoff <alex@artbanause.de>
2016-07-20 01:27:48 +0200

    next try

--- applet/src/display.c
+++ applet/src/display.c
@@ -14,16 +14,16 @@ void display_init_hook_1(void) {      // from 0x8033586 @ D003.020
   md380_read_config_3000();
   if ( md380_radio_config[0x1d] & 1)  // offset 0x1d from
                                       // 0x08033586 @ D003.020
-   Write_Data_2display(0x48);         // md390, no gps, config
+   Write_Data_2display(0x8);         //  orginal MD380
   else
-   Write_Data_2display(0x8);          // orginal MD380
+   Write_Data_2display(0x48);          // md390, no gps, config
 }
 
 void display_init_hook_2(void) {
   if ( md380_radio_config[0x1d] & 1)
-    Write_Data_2display(0x4f);
-  else
     Write_Data_2display(0x40);
+  else
+    Write_Data_2display(0x4f);
 }
 
 
