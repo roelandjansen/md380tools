7311f19a2d472838f2ec18646c5a3d617d1712bc
Alex Eickhoff <alex@artbanause.de>
2016-07-20 00:45:45 +0200

    next try

--- applet/src/display.c
+++ applet/src/display.c
@@ -11,6 +11,7 @@
 // workaround flipped display phenomenon
 
 void display_init_hook_1(void) {      // from 0x8033586 @ D003.020
+  md380_read_config_3000();
   if ( md380_radio_config[0x1d] & 1)  // offset 0x1d from
                                       // 0x08033586 @ D003.020
    Write_Data_2display(0x48);         // md390, no gps, config
