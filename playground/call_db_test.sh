8e6e453f05e8962f4ff2413765d9bcf3ca8612a2
Alex Eickhoff <alex@artbanause.de>
2016-07-27 01:51:45 +0200

    find call in db

--- playground/call_db_test.sh
+++ playground/call_db_test.sh
@@ -1,2 +1,2 @@
-rm dmr_id_db_test
- gcc -std=c99 -Wall -O2 -DDMR_USERDB_NOT_IN_FLASH -I ../applet/src/ ../applet/src/dmr_id_db.c id_db_test.c -o dmr_id_db_test && ./dmr_id_db_test
+rm -r call_db_test
+gcc -std=c99 -Wall -O2 -DDMR_USERDB_NOT_IN_FLASH -I ../applet/src/ ../applet/src/call_db.c call_db_test.c -o call_db_test && ./call_db_test
