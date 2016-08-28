f06dee527bf3eb056638894865ad6242e6fe0fa8
Simon IJskes <simon@ijskes.org>
2016-08-28 17:28:53 +0200

    experimental on poweron

--- applet/src/addl_config.c
+++ applet/src/addl_config.c
@@ -62,7 +62,8 @@ void init_global_addl_config_struct() {
   }
 
   // global_addl_config.experimental is intentionally not permanent
-  global_addl_config.experimental = 0;
+//  global_addl_config.experimental = 0;
+  global_addl_config.experimental = 1;
 }
 
 void init_global_addl_config_hook(void) {
